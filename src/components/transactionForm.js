15439d7cf1c21915b1dded3bf684577b9b6deb00
rogercodes1 <roger.ucsb@gmail.com>
2018-06-19 14:16:25 -0400

    setup transaction form front end.

--- src/components/transactionForm.js
+++ src/components/transactionForm.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react'
 //import PropTypes from 'prop-types'
 import { Button, Checkbox, Form, Input, Radio, Select, TextArea } from 'semantic-ui-react'
 
-// const options = [
-//   { key: 'i', text: 'Income', value: 'income' },
-//   { key: 'e', text: 'Expense', value: 'expense' },
-// ]
-// const categories = [
-//   { key: 'r', text: 'Restaurant', value: 'restaurant' },
-//   { key: 'phone', text: 'Phone Bill', value: 'phoneBill' },
-// ]
+const options = [
+  { key: 'i', text: 'Income', value: 'income' },
+  { key: 'e', text: 'Expense', value: 'expense' },
+]
+const categories = [
+  { key: 'r', text: 'Restaurant', value: 'restaurant' },
+  { key: 'phone', text: 'Phone Bill', value: 'phoneBill' },
+]
 
 class TransactionForm extends Component{
      constructor(){
@@ -29,21 +29,15 @@ class TransactionForm extends Component{
 
         <Form.Group >
 
-          <Form.Field width="5" control={Input} required type="date" label='Date' />
-          <Form.Field width="8" control={Input} label='Description' placeholder='Description' required/>
-          <Form.Field label='Category' control='select'>
-            <option value='restaurant'>Restaurant</option>
-            <option value='phoneBill'>Phone Bill</option>
-              </Form.Field>
-          <Form.Dropdown label='type' control='select'>
-            <option value='income'>Income</option>
-            <option value='expense'>Expense</option>
-            </Form.Dropdown>
+          <Form.Field  id="formDate"control={Input} required type="date" label='Date' />
+          <Form.Field id="formDescription" control={Input} label='Description' placeholder='Description' required/>
+          <Form.Select id="formCategory" label='Category' options={categories} placeholder='Category' />
+            <Form.Select id="formType" label='Type' options={options} placeholder='Type' required/>
 
+            <Form.Field id="button" control={Button}>Add</Form.Field>
         </Form.Group>
 
 
-        <Form.Field control={Button}>Submit</Form.Field>
       </Form>
   </div>
     )
--- src/components/transactionList.js
+++ src/components/transactionList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 //import PropTypes from 'prop-types'
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
 class TransactionList extends Component{
      constructor(){
@@ -12,7 +13,58 @@ class TransactionList extends Component{
     render() {
         return (
             <div>
-                <h2>Transaction list display component</h2>
+            <Table id="transactionTable" celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Date</Table.HeaderCell>
+                    <Table.HeaderCell>Description</Table.HeaderCell>
+                    <Table.HeaderCell>Category</Table.HeaderCell>
+                    <Table.HeaderCell>Type</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Label ribbon>First</Label>
+                    </Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                    <Table.Cell>Cell</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+
+                <Table.Footer>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan='4'>
+                      <Menu floated='right' pagination>
+                        <Menu.Item as='a' icon>
+                          <Icon name='chevron left' />
+                        </Menu.Item>
+                        <Menu.Item as='a'>1</Menu.Item>
+                        <Menu.Item as='a'>2</Menu.Item>
+                        <Menu.Item as='a'>3</Menu.Item>
+                        <Menu.Item as='a'>4</Menu.Item>
+                        <Menu.Item as='a' icon>
+                          <Icon name='chevron right' />
+                        </Menu.Item>
+                      </Menu>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+              </Table>
             </div>
         );
     }
