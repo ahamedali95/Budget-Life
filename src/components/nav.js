bfe53604a3881fb0819642f5b220e578446eb21e
rogercodes1 <roger.ucsb@gmail.com>
2018-06-19 15:35:42 -0400

    updated Nav bar to semantic UI.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,28 +1,59 @@
 import React, {Component} from 'react'
 //import PropTypes from 'prop-types'
+import { Menu, Icon, Segment, Dropdown } from 'semantic-ui-react'
 
 class Nav extends Component{
      constructor(){
         super();
 
         this.state={
-
+            activeItem: "home"
         }
     }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+
     render() {
-        return (
-            <div>
-                <h1>Nav bar</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>Transaction</li>
-                    <li>Bills</li>
-                    <li>Goals</li>
-                    <li>Settings</li>
-                </ul>
-            </div>
-        );
-    }
+    const { activeItem } = this.state
+
+    return (
+      <Menu>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        >
+          Home
+        </Menu.Item>
+
+        <Menu.Item name='transactions' active={activeItem === 'transactions'} onClick={this.handleItemClick}>
+          Transactions
+        </Menu.Item>
+
+        <Menu.Item
+          name='eventPlanning'
+          active={activeItem === 'eventPlanning'}
+          onClick={this.handleItemClick}
+        >
+          Event Planning
+        </Menu.Item>
+        <Menu.Menu position='right'>
+            <Dropdown  item icon="wrench" simple>
+                <Dropdown.Menu>
+                    <Dropdown.Item >
+                        Logout
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                        Profile
+                    </Dropdown.Item>
+
+                </Dropdown.Menu>
+
+            </Dropdown>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
 }
 
 export default Nav;
--- src/containers/transactionContainer.js
+++ src/containers/transactionContainer.js
@@ -11,6 +11,7 @@ class Transaction extends Component{
         super();
 
         this.state={
+            transactions: [],
             date: dateFormat(now,"mm,dd, yyyy"),
             description:null,
             category: null,
