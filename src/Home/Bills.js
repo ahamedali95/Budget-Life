6235b9cbb4a2fdf6b29798fbcc37744fbdf6da95
Ahamed <ahamedali95@gmail.com>
2018-06-22 13:25:26 -0400

    done

--- src/Home/Bills.js
+++ src/Home/Bills.js
@@ -7,15 +7,15 @@ const Bills = (props) => {
     const splittedDate = billObj.due_date.slice(0, 10).split("-");
     const formattedDate = [splittedDate[1], splittedDate[2], splittedDate[0]].join("/");
 
-    const category = props.categories.find((categoryObj) => {
-      return categoryObj.id === billObj.category_id
-    }).name;
+    // const category = props.categories.find((categoryObj) => {
+    //   return categoryObj.id === billObj.category_id
+    // }).name;
 
     return (
       <Table.Row key={billObj.id}>
         <Table.Cell>{billObj.description}</Table.Cell>
         <Table.Cell>${billObj.due_amount}</Table.Cell>
-        <Table.Cell>{category}</Table.Cell>
+
         <Table.Cell>{formattedDate}</Table.Cell>
       </Table.Row>
     );
@@ -29,7 +29,7 @@ const Bills = (props) => {
           <Table.Row>
             <th>Description</th>
             <th>Amount Due</th>
-            <th>Category</th>
+            
             <th>Due Date</th>
           </Table.Row>
         </Table.Header>
